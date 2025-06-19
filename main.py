cedae82cfe07953766c50d637bea486699b54253
Simon Makumi <simonmakumi5@gmail.com>
2025-06-19 14:07:07 +0300

    Placing buttons

--- main.py
+++ main.py
@@ -6,9 +6,51 @@ from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QHBox
 app = QApplication([])
 main_window = QWidget()
 main_window.setWindowTitle("Calculator App")
-main_window.resize(340,390)
+main_window.resize(200,400)
+
+# All objects/widgets
+
+text_box = QLineEdit()
+grid = QGridLayout()
+
+buttons = [
+    "7","8","9","/",
+    "4","5","6","*",
+    "1","2","3","-",
+    "0",".","=","+"
+    ]
+
+clear = QPushButton("Clear")
+delete = QPushButton("Del")
+
+
+row = 0
+col = 0
+
+for text in buttons:
+    button = QPushButton(text)
+    #button.clicked.connect(None)
+    grid.addWidget(button, row, col)
+    col +=1
+
+    if col > 3:
+        col = 0
+        row +=1
+
 
 # Design
+master_layout = QVBoxLayout()
+master_layout.addWidget(text_box)
+master_layout.addLayout(grid)
+
+button_row = QHBoxLayout()
+button_row.addWidget(clear)
+button_row.addWidget(delete)
+
+master_layout.addLayout(button_row)
+
+main_window.setLayout(master_layout)
+
 
 # Show/Run
 main_window.show()
